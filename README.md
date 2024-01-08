3f6723f1760771e98538fed4d9e69a694534d534
ASHMIT NIGAM <28896262+ashmit1294@users.noreply.github.com>
2024-01-08 15:17:24 +0530

    Initail Youtube project starting

---
+++ README.md
@@ -0,0 +1,14 @@
+# header
+    -   hanburger menu  
+            - opens categories
+            - when closes shows tags for those categories
+    -   logo
+    -   Youtube text label
+    -   search bar
+    -   search icon 
+# Sidebar  
+# body
+    -> buttonlist
+    -> Vedio Container
+    -> Vedio card component
+# footer
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'; //without this all base , components and utilities files of tailwind wont be called!
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Body />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
