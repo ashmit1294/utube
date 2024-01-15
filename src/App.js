88d0ee6dc1b4501ee5b69d9c29af36255e582a4c
ASHMIT NIGAM <28896262+ashmit1294@users.noreply.github.com>
2024-01-16 02:56:00 +0530

    jan162024-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css'; //without this all base , components and utilities files of tailwind wont be called!
 import Header from "./components/Header";
 import Body from "./components/Body";
-import Footer from "./components/Footer";
-
 function App() {
   return (
     <div>
