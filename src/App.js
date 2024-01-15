a6ef5ba72efbae513c401de812b94254da86e7e3
ASHMIT NIGAM <28896262+ashmit1294@users.noreply.github.com>
2024-01-16 04:25:45 +0530

    redux store added with appSlice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css'; //without this all base , components and utilities files of tailwind wont be called!
 import Header from "./components/Header";
 import Body from "./components/Body";
+import store from './utils/store';
+import { Provider } from 'react-redux';
 function App() {
   return (
+    <Provider store={store}>
     <div>
       <Header/>
       <Body />
     </div>
+    </Provider>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Sidebar() {
+  const isMenuOpen=useSelector(store=>store.app.isMenuOpen);
+  if(!isMenuOpen) return null;
   return (
     <div className='p-5 shadow-lg w-48' >
-      <h1 className='font-bold'>
+      <ul>
+        <li>Home</li>
+        <li>Sports</li>
+        <li>Vedios</li>
+        <li>Live  </li>
+      </ul>
+      <h1 className='font-bold pt-5'>
         Subscriptions
       </h1>
       <ul>
